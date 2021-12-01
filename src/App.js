6a830aeb5c88e0d003eef13b04e7358f4b33a5d1
Marian-Janolek <m.janolek@gmail.com>
2021-12-01 18:39:36 +0100

    add login and register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from './pages/home/Home';
 import './app.scss';
+import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Login />
     </div>
   );
 }
